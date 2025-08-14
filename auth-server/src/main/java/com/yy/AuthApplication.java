921bcbc314ef7578bc82c4bbdda8165e457a96d8
15525438831 <15525438831@163.com>
2025-08-14 15:08:20 +0800

    初始话提交

---
+++ auth-server/src/main/java/com/yy/AuthApplication.java
@@ -0,0 +1,19 @@
+package com.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 授权服务启动类
+ * @ClassName AuthApplication
+ * @Author yangfeng
+ * @Date 2025/8/13 16:52
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
